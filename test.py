68d7fea42158e634963fb5e711335977d64e3518
CorpsEater <125800808+CorpsEater@users.noreply.github.com>
2023-02-19 17:57:58 +0500

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-number = input("Введи номер телефона\n")
-while True:
-    if len(str(number)) == 11 and str(number)[0] in ('7','8'):
-        print('Проверка успешная')
-        break
-    else:
-        number = input(f'Кол-во символов: {len(str(number))}\nПервый символ: {str(number)[0]}\n\nНомер не корректный, повтори попытку\n')
